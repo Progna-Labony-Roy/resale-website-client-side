d3fb35817050217b34792724a83799f0841001cc
Progna_Labony_Roy <labony.progna@gmail.com>
2022-11-24 15:13:18 +0600

    Signup and Login page added

--- src/Pages/Home/Signup/Signup.js
+++ src/Pages/Home/Signup/Signup.js
@@ -1,12 +1,93 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm
+  ();
+
+  const [signupError, setSignupError] = useState()
+   
+  const handleSignup =() =>{
+
+  }
+
     return (
-        <div>
-       
-      </div>
-    );
+      <div className="flex justify-center">
+      <form onSubmit={handleSubmit(handleSignup)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <div className="form-control w-full max-w-xs my-5">
+          <h2 className="text-center text-4xl font-bold mb-8">Sign Up</h2>
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            {...register("name", { required: "Please write your name" })}
+            type="text"
+            className="input input-bordered w-full max-w-xs my-2"
+          />
+          {errors.name && (
+            <span className="text-red-500">This field is required</span>
+          )}
+
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            {...register("email", { required: "Email is required" })}
+            type="text"
+            name="email"
+            className="input input-bordered w-full max-w-xs my-2"
+          />
+          {errors.email && (
+            <span className="text-red-500">{errors.email.message}</span>
+          )}
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            {...register("password", {
+              required: "Please write your password",
+              minLength: {
+                value: 6,
+                message: "Password must be atleast 6 characters",
+              },
+            })}
+            type="password"
+            className="input input-bordered w-full max-w-xs mt-2"
+          />
+          {errors.password && (
+            <span className="text-red-500">{errors.password.message}</span>
+          )}
+        </div>
+        <input
+          type="submit"
+          value="SignUp"
+          className="btn btn-success my-3 w-full"
+        />
+         <div>
+          {
+            <p className="text-red-500 py-1"></p>
+          }
+        </div>
+        <p>
+          Already have an account{" "}
+          <Link to="/login" className="text-green-500">
+            Please Log in
+          </Link>{" "}
+        </p>
+        <div className="divider">OR</div>
+        <button className="btn btn-outline btn-accent w-full">
+          CONTINUE WITH GOOGLE
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div></div>;
-};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [loginError ,setLoginError] = useState('');
+
+  const handleLogin =() =>{
+
+  }
+
+
+  return (
+    <div className="flex justify-center">
+    <form onSubmit={handleSubmit(handleLogin)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <div className="form-control w-full max-w-xs my-5">
+        <h2 className="text-center text-4xl font-bold mb-8">Login</h2>
+        <label className="label">
+          <span className="label-text">Email</span>
+        </label>
+        <input
+          {...register("email" ,{ required: "Email is required" }) }
+          type="text"
+          className="input input-bordered w-full max-w-xs my-2"
+        />
+        {errors.email && <span className="text-red-500">{errors.email.message}</span>}
+        <label className="label">
+          <span className="label-text">Password</span>
+        </label>
+        <input
+          {...register("password",{ required: "Wrong password" })}
+          type="password"
+          className="input input-bordered w-full max-w-xs mt-2"
+        />
+        {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+      </div>
+      <label className="label">
+        <span className="label-text">Forget password</span>
+      </label>
+      
+      <input type="submit" value='login' className="btn btn-success my-3 w-full" />
+      <div>
+        {
+          <p className="text-red-500 py-1"></p>
+        }
+      </div>
+      <p>New to doctors portal? <Link to='/signup' className="text-orange-500">Create an account</Link> </p>
+      <div className="divider">OR</div>
+      <button className="btn btn-outline btn-accent w-full">CONTINUE WITH GOOGLE</button>
+    </form>
+  </div>
+  )
+      }
 
 export default Login;
